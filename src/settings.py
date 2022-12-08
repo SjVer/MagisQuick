3ed85db2f4a34c68ffe05d8bb41b679ab8fb6343
SjVer <sjoerd@marsenaar.com>
2022-12-08 12:18:22 +0100

    upload

--- src/settings.py
+++ src/settings.py
@@ -75,13 +75,8 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'SjVer$db',
-        'USER': 'SjVer',
-        'PASSWORD': 'mysql1234',
-        'HOST': 'SjVer.mysql.pythonanywhere-services.com',    
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
